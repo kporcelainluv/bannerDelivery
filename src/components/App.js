2d6c2270d7d4c886c09da645269b7234cf26a22d
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-05-01 20:05:24 +0300

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box, Card, Image, Heading, Text } from "rebass";
+
+import GlobalTheme from "../theme/GlobalTheme";
 
 export const App = () => {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <GlobalTheme />
+      <Box width={"100%"}>
+        <Heading as="h1">{"Banner delivery"}</Heading>
+      </Box>
+    </div>
+  );
 };
-
-export default App;
