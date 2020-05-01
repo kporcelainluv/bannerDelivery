e5fa60d45522c88fe5e479fadfa2cff2d3977a46
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-05-01 19:24:21 +0300

    add styling theme

---
+++ src/theme/dark.js
@@ -0,0 +1,26 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const darkTheme = {
+  colors: {
+    grey000: "#FFFFFE",
+    grey100: "#F2F3F6",
+    grey200: "#9C9DA0",
+    grey300: "#97999E",
+    grey400: "#43414D",
+    grey500: "#3F4C5C",
+    grey600: "#3f4c5c",
+    grey700: "#1D2733",
+    orange100: "#FF8E3C",
+    orange200: "#D67935",
+    red100: "#EB5760",
+    gradient1: "linear-gradient(180deg, #2A2A2A 0%, #0D0D0D 100%)",
+    gradient2:
+      "linear-gradient(0deg, rgba(63, 76, 92, 0.7), rgba(63, 76, 92, 0.7)), #0D0D0D"
+  },
+  fonts: {
+    xs: "14px",
+    s: "16px",
+    m: "18px",
+    l: "32px"
+  }
+};
