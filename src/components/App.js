6a49b78640198a75c79e18f514308b890f1e219c
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-05-03 18:33:32 +0300

    add customers state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalTheme from "../theme/GlobalTheme";
 import { ThemeProvider } from "styled-components";
 
@@ -7,12 +7,19 @@ import { Header } from "./Header";
 import { Dashboard } from "./Dashboard";
 
 export const App = () => {
+  const [customers, setCustomers] = useState([]);
+
+  const addCustomer = name => {
+    console.log(name);
+    setCustomers([...customers, { name: name }]);
+  };
+
   return (
     <div className="App">
       <GlobalTheme />
       <ThemeProvider theme={darkTheme}>
         <Header />
-        <Dashboard />
+        <Dashboard addCustomer={addCustomer} />
       </ThemeProvider>
     </div>
   );
