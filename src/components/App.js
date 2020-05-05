ab0cde3ebcdf42b4970ad23f0235210bed3eed87
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-05-05 13:31:54 +0300

    add customers arr, add removeCustomer, markCompleted funcs

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,46 @@ import { nanoid } from "nanoid";
 
 import { darkTheme } from "../theme/dark";
 import { Header } from "./Header";
-import { DashboardEmpty } from "./DashboardEmpty";
+import { Dashboard } from "./Dashboard";
 
 export const App = () => {
-  const [customers, setCustomers] = useState([]);
+  const [customers, setCustomers] = useState([
+    { id: nanoid(), name: "Burger King", status: "active" },
+    { id: nanoid(), name: "Mama Pizza", status: "completed" },
+    { id: nanoid(), name: "Domofond", status: "active" },
+    { id: nanoid(), name: "Sberbank", status: "completed" }
+  ]);
 
   const addCustomer = name => {
-    setCustomers(c => [...c, { name: name, id: nanoid() }]);
+    setCustomers(c => [...c, { name: name, id: nanoid(), status: "active" }]);
   };
-  console.log({ customers });
+
+  const removeCustomer = customer => {
+    const updatedCustomers = customers.filter(c => c.id !== customer.id);
+    setCustomers(updatedCustomers);
+  };
+
+  const markCompleted = customer => {
+    setCustomers(
+      customers.map(c => {
+        if (c.id === customer.id) {
+          return { ...c, status: "completed" };
+        }
+        return c;
+      })
+    );
+  };
   return (
     <div className="App">
       <GlobalTheme />
       <ThemeProvider theme={darkTheme}>
         <Header />
-        <DashboardEmpty addCustomer={addCustomer} />
+        <Dashboard
+          addCustomer={addCustomer}
+          removeCustomer={removeCustomer}
+          markCompleted={markCompleted}
+          customers={customers}
+        />
       </ThemeProvider>
     </div>
   );
