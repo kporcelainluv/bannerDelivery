2de3ca8aeba74f55b8583f23a40eddf5b0a2f16d
x <zhukovaksusha@gmail.com>
2020-06-12 17:30:08 +0300

    Add Icon component

---
+++ src/components/Icon.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Svg = styled.svg`
+    &:hover {
+        fill:  ${p => p.theme.colors.orange100};
+    }
+`
+
+export const Icon = ({ height, width, name, viewbox, fill }) => {
+    return (
+        <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            height={height}
+            width={width}
+            viewBox={viewbox ? viewbox : `0 0 ${height} ${width}`}
+            fill={fill? fill : "none"}
+        >
+            <use xlinkHref={`/icons/sprite.svg#${name}`} />
+        </Svg>
+    );
+};
